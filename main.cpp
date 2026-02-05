fc8d634cc331d6f7e487f73bf64a005bc4a3c114
Brainfeed-1996 <121355278+Brainfeed-1996@users.noreply.github.com>
2026-02-05 13:32:50 +0100

    refactor: update main to use the new NeuralNetwork class

--- main.cpp
+++ main.cpp
@@ -1,52 +1,32 @@
+#include "include/NeuralNetwork.hpp"
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-
-// Sigmoid activation function
-double sigmoid(double x) {
-    return 1.0 / (1.0 + exp(-x));
-}
-
-// Derivative of sigmoid
-double sigmoid_derivative(double x) {
-    return x * (1.0 - x);
-}
-
-class Neuron {
-public:
-    double value;
-    double activated_value;
-    double derived_value;
-    std::vector<double> weights;
-
-    Neuron(int prev_layer_size) {
-        for (int i = 0; i < prev_layer_size; i++) {
-            weights.push_back(((double)rand() / RAND_MAX) * 2.0 - 1.0);
-        }
-    }
-};
-
-class Layer {
-public:
-    std::vector<Neuron> neurons;
-
-    Layer(int size, int prev_layer_size) {
-        for (int i = 0; i < size; i++) {
-            neurons.push_back(Neuron(prev_layer_size));
-        }
-    }
-};
 
 int main() {
-    srand(time(0));
-    std::cout << "Initializing Neural Network..." << std::endl;
-    
-    // Example topology: 2 inputs, 3 hidden, 1 output
-    Layer hidden_layer(3, 2);
-    Layer output_layer(1, 3);
+    // XOR Problem
+    std::vector<int> topology = {2, 4, 1};
+    NeuralNetwork nn(topology, 0.5);
 
-    std::cout << "Network initialized. Ready for training loops." << std::endl;
+    std::vector<Eigen::VectorXd> inputs = {
+        (Eigen::VectorXd(2) << 0, 0).finished(),
+        (Eigen::VectorXd(2) << 0, 1).finished(),
+        (Eigen::VectorXd(2) << 1, 0).finished(),
+        (Eigen::VectorXd(2) << 1, 1).finished()
+    };
+
+    std::vector<Eigen::VectorXd> targets = {
+        (Eigen::VectorXd(1) << 0).finished(),
+        (Eigen::VectorXd(1) << 1).finished(),
+        (Eigen::VectorXd(1) << 1).finished(),
+        (Eigen::VectorXd(1) << 0).finished()
+    };
+
+    std::cout << "Training for XOR..." << std::endl;
+    nn.train(inputs, targets, 2000);
+
+    std::cout << "\nTesting XOR:" << std::endl;
+    for (const auto& in : inputs) {
+        std::cout << in.transpose() << " => " << nn.feedForward(in).transpose() << std::endl;
+    }
+
     return 0;
 }
